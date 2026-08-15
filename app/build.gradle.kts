c7593f85ea3d04db3b41dd173c5e2430da842740
Vexta Project <vextaproject@users.noreply.github.com>
2026-08-15 14:48:46 +0000

    Set release version to v0.6.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "org.vextaproject.wallet"
         minSdk = 26
         targetSdk = 36
-        versionCode = 35
-        versionName = "0.6.5"
+        versionCode = 34
+        versionName = "0.6.4"
     }
 
     signingConfigs {
